964d1f1b50a6ef9e7278fdaf91c4f6943521eb54
MariuszKuich <mariuszkuich@outlook.com>
2022-06-06 08:41:12 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Starfighter
+##### Simple Android game made with LibGDX based on these tutorials:
+https://www.youtube.com/watch?v=DK1sGc4rOT4&list=PLfd-5Q3Fwq0WKrkEKw12nqpfER3MG5_Wi
---
+++ desktop/src/pl/mariuszk/starfighter/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package pl.mariuszk.starfighter;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import pl.mariuszk.starfighter.StarfighterGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Starfighter");
+		new Lwjgl3Application(new StarfighterGame(), config);
+	}
+}
